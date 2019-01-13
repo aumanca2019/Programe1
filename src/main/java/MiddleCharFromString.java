393d58252541ccf0fac5a56f310e661668e81c56
aumanca2019 <ancalumi68@yahoo.com>
2019-01-13 22:48:37 +0200

    kLargest2

--- src/main/java/MiddleCharFromString.java
+++ src/main/java/MiddleCharFromString.java
@@ -1,16 +1,25 @@
 public class MiddleCharFromString {
     public static void main(String[] args) {
+        String testString = "aaafgtbaaa";
+        String testString2 = "aagftaXjsjaaa";
+        displayMiddleChar(testString);
+        displayMiddleChar(testString2);
     }
-}
+    private static void displayMiddleChar(String testString){
+        int length = testString.length();
+        System.out.println("String length = " + length);
 
-    //String testString = "aaabaaa";} //  displayMiddleChar(testString);
-    //}
-    //private static void displayMiddleChar(String testString){
-        //int length = testString.length();
-        //System.out.println("String length = " + length);
+        if (length%2 != 0) {
+            int position = length / 2;
+            System.out.println(testString.charAt(position));
+        }else {
+            char c2 = testString.charAt(length / 2);
+            char c1 = testString.charAt(length / 2 -1);
+            System.out.println(String.valueOf(c1 + String.valueOf(c2)));
 
-       // if (length%2!=0){
-       //     int position = length/2;System.out.println(testString.charAt(position));
-    //    }else {
-  //  }
-//}
+            }
+        }
+    }
+
+
+
