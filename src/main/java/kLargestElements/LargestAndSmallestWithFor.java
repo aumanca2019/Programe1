4458959718958c19ae6d84540f097a5b4e103211
aumanca2019 <ancalumi68@yahoo.com>
2019-01-13 14:31:17 +0200

    kLargest2

--- src/main/java/kLargestElements/LargestAndSmallestWithFor.java
+++ src/main/java/kLargestElements/LargestAndSmallestWithFor.java
@@ -1,25 +1,60 @@
 package kLargestElements;
 
+
 public class LargestAndSmallestWithFor {
     public static void main(String[] args) {
-        System.out.println("LargestElement = " + largestElement());
+
+        largestElement();
     }
 
-
+    static int largestElement;
     static int arr[] = new int[]{45, 235, 98, 56, 34, 67, 14, 89};
 
-        public static int largestElement() {
+    public static int largestElement() {
 
-            int max = arr[0];
+        int max1 = arr[0];
+        int max2 = arr[0];
+        int max3 = arr[0];
 
 
-            for (int i = 0; i < arr.length; i++)
-                if (max < arr[i])
-                    max = arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (max1 < arr[i]) {
+                max1 = arr[i];
 
-                return max;
+            } else if (arr[i] > max2) {
+                max2 = arr[i];
+
+            } else if (arr[i] > max3) {
+                max3 = arr[i];
+
+
+            }
+
+
         }
+        System.out.println("largest elements " + " " + max1 + " " + max2 + " " + max3);
+
+        return largestElement;
     }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
