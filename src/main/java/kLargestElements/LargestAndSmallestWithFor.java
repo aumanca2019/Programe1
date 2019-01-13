44f59e7186981ce6010f1ce0342baa7f8527727d
aumanca2019 <ancalumi68@yahoo.com>
2019-01-13 02:40:57 +0200

    kLargest2

--- src/main/java/kLargestElements/LargestAndSmallestWithFor.java
+++ src/main/java/kLargestElements/LargestAndSmallestWithFor.java
@@ -1,43 +1,35 @@
 package kLargestElements;
 
 public class LargestAndSmallestWithFor {
-
-    private static Object getMaxElement;
-
     public static void main(String[] args) {
-        int arr[] = {234, 56, 43, 5, 78, 89, 100};
+        System.out.println("LargestElement = " + largestElement());
+    }
 
 
-            int max1 = arr[0];
-            int max2 = arr[0];
-            int max3 = arr[0];
+    static int arr[] = new int[]{45, 235, 98, 56, 34, 67, 14, 89};
 
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] > max1) {
-                    max1 = arr[i];
-                    System.out.println(" max1 " + max1);
-                } else if ((arr[i]!= max1) && (arr[i]> max2)){
-                    max2 = arr[i];
-                   System.out.println(" max2 " + max2);
-                } else if ((arr[i]!= max1)&& (arr[i]!= max2)){
-                    max3 = arr[i];
-                    System.out.println(" max3 " + max3);
-                }
+        public static int largestElement() {
+
+            int max = arr[0];
 
 
+            for (int i = 0; i < arr.length; i++)
+                if (max < arr[i])
+                    max = arr[i];
 
-
-                }
-
-
+                return max;
         }
-
     }
 
 
 
-        //}
-      //  return max1;
+
+
+
+
+
+
+
 
 
 
