ba5f1c5d64e0378355db80f0c09d2874ad625d5b
aumanca2019 <ancalumi68@yahoo.com>
2019-01-13 02:41:45 +0200

    incercari

---
+++ src/main/java/MiddleCharFromString.java
@@ -0,0 +1,16 @@
+public class MiddleCharFromString {
+    public static void main(String[] args) {
+    }
+}
+
+    //String testString = "aaabaaa";} //  displayMiddleChar(testString);
+    //}
+    //private static void displayMiddleChar(String testString){
+        //int length = testString.length();
+        //System.out.println("String length = " + length);
+
+       // if (length%2!=0){
+       //     int position = length/2;System.out.println(testString.charAt(position));
+    //    }else {
+  //  }
+//}
