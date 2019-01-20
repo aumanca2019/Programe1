0b40bd5c71b84f23694534b863af6b6c87908afe
aumanca2019 <ancalumi68@yahoo.com>
2019-01-20 14:33:36 +0200

    upload 20.01.2019

--- src/main/java/kLargestElements/LargestAndSmallestWithFor.java
+++ src/main/java/kLargestElements/LargestAndSmallestWithFor.java
@@ -10,14 +10,17 @@ public class LargestAndSmallestWithFor {
     static int largestElement;
     static int arr[] = new int[]{45, 235, 98, 56, 34, 67, 14, 89};
 
+
     public static int largestElement() {
 
         int max1 = arr[0];
         int max2 = arr[0];
         int max3 = arr[0];
+        int max4 = arr[0];
+        int max5 = arr[4];
 
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++)
             if (max1 < arr[i]) {
                 max1 = arr[i];
 
@@ -27,18 +30,28 @@ public class LargestAndSmallestWithFor {
             } else if (arr[i] > max3) {
                 max3 = arr[i];
 
+            } else if (arr[i] > max4) {
+                max4 = arr[i];
+
+
+            } else if (arr[i] > max5) {
+                max5 = arr[i];
+
 
             }
 
-
-        }
         System.out.println("largest elements " + " " + max1 + " " + max2 + " " + max3);
+        System.out.println();
+        System.out.println("Laregst elements " + max4 + " " + max5);
 
         return largestElement;
     }
 
 
 }
+
+
+
 
 
 
